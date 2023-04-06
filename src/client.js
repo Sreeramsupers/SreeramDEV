c7a5a9db4f0c277b2e2814263aacc02679abd513
Sreeram_Supers <sreerambhavanspkd@gmail.com>
2023-04-06 20:02:16 +0530

    Migrated from CRA to VITE

--- src/client.js
+++ src/client.js
@@ -2,11 +2,11 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-	projectId: process.env.REACT_APP_SANITY_PRO_ID,
-	dataset: process.env.REACT_APP_SANITY_DATASET,
-	apiVersion: process.env.REACT_APP_SANITY_API_VERSION,
+	projectId: import.meta.env.VITE_SANITY_PRO_ID,
+	dataset: import.meta.env.VITE_SANITY_DATASET,
+	apiVersion: import.meta.env.VITE_SANITY_API_VERSION,
 	useCdn: true,
-	token: process.env.REACT_APP_SANITY_TOKEN,
+	token: import.meta.env.VITE_SANITY_TOKEN,
 });
 
 const builder = imageUrlBuilder(client);
